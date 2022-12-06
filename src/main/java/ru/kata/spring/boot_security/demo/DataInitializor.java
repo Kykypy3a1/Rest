a831af90c555edd153852f88e95adda49b6ce701
Kykypy3a1 <108570735+Kykypy3a1@users.noreply.github.com>
2022-12-06 21:30:16 +0300

    up

--- src/main/java/ru/kata/spring/boot_security/demo/DataInitializor.java
+++ src/main/java/ru/kata/spring/boot_security/demo/DataInitializor.java
@@ -1,28 +1,33 @@
 package ru.kata.spring.boot_security.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.repositories.UserRepository;
+import ru.kata.spring.boot_security.demo.services.UserService;
 
 import javax.annotation.PostConstruct;
 import java.util.Set;
 
 @Component
+@Transactional(readOnly = false)
 public class DataInitializor {
-    private final UserRepository userRepository;
+    private final UserService userService;
 
-    public DataInitializor(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    @Autowired
+    public DataInitializor(UserService userService) {
+        this.userService = userService;
     }
 
     //Добавление пользователя для теста функционала
     @PostConstruct
     public void createTestAdmin() {
-        if (userRepository.findAll().isEmpty()) {
+        if (userService.getAllUsers().isEmpty()) {
             User admin = new User("admin", "admin", 20, "$2a$12$LEw3I8C75TzmGXw1URpOZO7NMCGhMJDRmCRPflBzZNJjzzWmRh6N6");
             admin.setRoles(Set.of(new Role(1, "ROLE_ADMIN"), new Role(2, "ROLE_USER")));
-            userRepository.save(admin);
+            userService.saveUser(admin,"ROLE_ADMIN");
         }
     }
 }
